fb56bd63225bc77e6b20328774684e7cecab079b
arbaaz <mirzaarbaaz123@gmail.com>
2022-08-11 18:27:31 -0400

    move repository from a fork since my commits weren't being recorded

---
+++ README.md
@@ -0,0 +1,11 @@
+# [Calculator](https://am-png.github.io/calculator/)
+Repository of my calculator project
+
+## How It's Made
+**Tech used:** HTML, CSS and JavaScript
+
+## Optimizations
+I refactor as much as I can as long as the code is readable/easy to understand.
+
+## Lessons Learned:
+I learned many unique algorithms as well as the fact that I need to have a growth mindset as I have a long way to go.
---
+++ js/main.js
@@ -0,0 +1,77 @@
+let current = document.querySelector(".current")
+let previous = document.querySelector(".previous")
+
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+    
+        let action = button.dataset.action
+        let buttonContent = button.textContent
+        let curDisplay = current.textContent
+        let preDisplay = previous.textContent
+
+        let operator = localStorage.getItem('operator')
+
+        // if it's a number
+        if (!action) {
+            if (curDisplay.includes('.') && buttonContent === '.') {
+                current.textContent += ''
+            } else {
+                current.textContent += buttonContent
+            }
+        // if it's an action key that is not delete, equal or clear
+        } else if (action !== 'delete' && action !== 'equal' && action !== 'clear') {
+            previous.textContent = curDisplay
+            current.textContent = ""
+            localStorage.setItem('operator', buttonContent)
+        }
+
+        // operate() when both displays have values and action key isn't equal or delete
+        if (preDisplay !== "" && curDisplay !== "" && action == 'add' || action == "subtract" || action === 'divide' || action === 'multiply') {
+            previous.textContent = operate()
+            current.textContent = ''
+        }
+        
+        // operate() when both displays have values and action key is equal
+        if (preDisplay !== "" && curDisplay !== "" && action === 'equal') {
+            previous.textContent = ""
+            current.textContent = operate()
+        }
+
+        function operate(n1, n2) {
+
+            n1 = preDisplay
+            n2 = curDisplay
+            let result
+
+            if (operator ==='/') divide()
+            if (operator === '*') multiply()
+            if (operator === '+') add()
+            if (operator === '-') subtract()
+
+            function divide() {
+                return result = +n1 / +n2
+            }
+
+            function multiply() {
+                return result = +n1 * +n2
+            }
+
+            function add() {
+                return result = +n1 + +n2
+            }
+
+            function subtract() {
+                return result = +n1 - +n2
+            }
+            return result
+        }
+
+        if (action === 'clear') {
+            document.querySelectorAll("p").forEach(p => p.textContent = "")
+        }
+        
+        if (action === 'delete') {
+            current.textContent = curDisplay.slice(0,-1)
+        }
+    })
+})
