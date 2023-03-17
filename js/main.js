7ce142dc154954c051f60138ec06682b6148b543
arbaaz <mirzaarbaaz123@gmail.com>
2023-03-17 14:28:03 -0400

    add/change data attributes of numbers and operators; change calculation functions from traditional function expression to arrow functions

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ document.querySelectorAll('button').forEach(button => {
     button.addEventListener('click', () => {
     
         let action = button.dataset.action
+        let operators = button.dataset.operator
         let buttonContent = button.textContent
         let curDisplay = current.textContent
         let preDisplay = previous.textContent
@@ -12,21 +13,21 @@ document.querySelectorAll('button').forEach(button => {
         let operator = sessionStorage.getItem(button)
 
         // if it's a number
-        if (!action) {
+        if (!operators) {
             if (curDisplay.includes('.') && buttonContent === '.') {
                 current.textContent += ''
             } else {
                 current.textContent += buttonContent
             }
         // if it's an action key that is not delete, equal or clear
-        } else if (action !== 'delete' && action !== 'equal' && action !== 'clear') {
+        } else if (operators) {
             previous.textContent = curDisplay
             current.textContent = ""
             sessionStorage.setItem(button, buttonContent)
         }
 
         // operate() when both displays have values and action key isn't equal or delete
-        if (preDisplay !== "" && curDisplay !== "" && (action == 'add' || action == "subtract" || action === 'divide' || action === 'multiply') ) {
+        if (preDisplay !== "" && curDisplay !== "" && (operators) ) {
             previous.textContent = operate()
             current.textContent = ''
         }
@@ -39,8 +40,8 @@ document.querySelectorAll('button').forEach(button => {
 
         function operate(n1, n2) {
 
-            n1 = preDisplay
-            n2 = curDisplay
+            n1 = +preDisplay
+            n2 = +curDisplay
             let result
 
             if (operator ==='/') divide()
@@ -48,21 +49,14 @@ document.querySelectorAll('button').forEach(button => {
             if (operator === '+') add()
             if (operator === '-') subtract()
 
-            function divide() {
-                return result = +n1 / +n2
-            }
+            const divide = () => result = n1 / n2
 
-            function multiply() {
-                return result = +n1 * +n2
-            }
+            const multiply = () => result = n1 * n2
 
-            function add() {
-                return result = +n1 + +n2
-            }
+            const add = () => result = n1 + n2
 
-            function subtract() {
-                return result = +n1 - +n2
-            }
+            const subtract = () => result = n1 - n2
+            
             return +result.toFixed(2)
         }
 
